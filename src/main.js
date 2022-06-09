4a97efc7656d415c42cd3afb63813dd5c73dfe97
myself <1819127584@qq.com>
2022-06-10 01:06:38 +0800

    第一次提交

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import axios from 'axios'
+import App from './App'
+import router from './router'
+import qs from 'qs'
+import VueAxios from 'vue-axios'
+
+Vue.config.productionTip = false
+Vue.prototype.$axios = axios //全局注册，使用方法为:this.$axios
+Vue.prototype.qs = qs //全局注册，使用方法为:this.qs
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: {
+    App
+  },
+  template: '<App/>'
+})
---
+++ src/router/index.js
@@ -0,0 +1,71 @@
+import Vue from 'vue'
+// import VueRouter from 'vue-router'
+// import LoginVue from '../components/LoginVue.vue'
+// import RegisterVue from '../components/RegisterVue.vue'
+// import PocketBook from '../components/PocketBook.vue'
+// import InfoVue from '../components/InfoVue.vue'
+// import UpdateVue from '../components/UpdateVue.vue'
+// import TestVue from '../components/TestVue.vue'
+import HelloWorld from '../components/HelloWorld.vue'
+import Index from '../components/index.vue'
+// import TheAdd from '../components/TheAdd.vue'
+import ElementUI from 'element-ui'
+
+Vue.use(VueRouter);
+Vue.use(ElementUI);
+
+// 创建路由
+export default new VueRouter({
+	routes: [
+		// {
+		// 	path: '/',
+		// 	redirect: '/hello'
+		// },
+		// {
+		// 	path: '/index',
+		// 	name: 'Index',
+		// 	component: Index,
+		// },
+		// {
+		// 	path: '/login',
+		// 	name: 'LoginVue',
+		// 	component: LoginVue
+		// },
+		// {
+		// 	path: '/register',
+		// 	name: 'RegisterVue',
+		// 	component: RegisterVue
+		// },
+		// {
+		// 	path: '/pocketbook',
+		// 	name: 'PocketBook',
+		// 	component: PocketBook
+		// },
+		// {
+		// 	path: '/info',
+		// 	name: 'InfoVue',
+		// 	component: InfoVue
+		// },
+		// {
+		// 	path: '/update',
+		// 	name: 'UpdateVue',
+		// 	component: UpdateVue
+		// },
+		// {
+		// 	path: '/test',
+		// 	name: 'TestVue',
+		// 	component: TestVue
+		// },
+		// {
+		// 	path: '/hello',
+		// 	name: 'HelloWorld',
+		// 	component: HelloWorld
+		// }
+		// {
+		// 	path: '/add',
+		// 	name: 'TheAdd',
+		// 	component: TheAdd
+		// }
+	],
+	mode: 'hash'
+})
